c47968c8de0a10be0b79aa74fe5e16c7ae2aad64
Chang Liu <changbobbyliu@gmail.com>
2022-11-26 20:07:59 -0800

    1

--- src/ui/widgets/header/Header.tsx
+++ src/ui/widgets/header/Header.tsx
@@ -1,7 +1,10 @@
 import { ConnectWallet } from "@thirdweb-dev/react";
+import Image from "next/image";
 import { FC, useState } from "react";
 import { HambugerMenu } from "./HamburgerMenu";
 import { DrawerNav } from "./nav/DrawerNav";
+import { NavItemsList } from "./nav/NavItemsList";
+import logo from "@/assets/logo.svg";
 
 export const Header: FC = () => {
 	const [menuOpen, setMenuOpen] = useState(false);
@@ -11,8 +14,16 @@ export const Header: FC = () => {
 				<div className="flex items-center flex-grow">
 					<HambugerMenu open={menuOpen} setOpen={setMenuOpen} />
 					<DrawerNav isOpen={menuOpen} close={() => setMenuOpen(false)} />
+					<div className="items-center hidden md:flex">
+						<div className="mr-2">
+							<Image src={logo} alt="logo" width={32} height={32} />
+						</div>
+						<ul className="flex space-x-1">
+							<NavItemsList />
+						</ul>
+					</div>
 				</div>
-				<ConnectWallet />
+				<ConnectWallet accentColor="rgb(30 41 59)" />
 			</div>
 		</header>
 	);
